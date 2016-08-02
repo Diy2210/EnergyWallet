48e3de8fb4e7d9722df36a4df7d9b98b69ba2969
Diy2210 <Diy2210@mac-mini-diy2210.local>
2016-08-02 21:57:20 +0300

    Initial Commit

---
+++ EnergyWallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EnergyWallet
+//
+//  Created by Diy2210 on 02.08.16.
+//  Copyright © 2016 Diy2210. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
